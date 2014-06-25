d3e402c3fae6c3f0d8a5129426d465c27dd749b6
Derek Richard and Nicholas Calugar <ncalugar@pivotallabs.com>
2014-06-24 17:36:18 -0700

    Server that executes galera healthcheck
    * TODO: Need to backfill tests
    
    [#71193814]

---
+++ src/github.com/cloudfoundry-incubator/galera-healthcheck/server/server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/cloudfoundry-incubator/galera-healthcheck/healthcheck"
+)
+
+var serverPort = flag.Int(
+	"port",
+	8080,
+	"Specifies the port of the healthcheck server",
+)
+
+var mysqlUser = flag.String(
+	"user",
+	"root",
+	"Specifies the MySQL user to connect as",
+)
+
+var mysqlPassword = flag.String(
+	"password",
+	"",
+	"Specifies the MySQL password to connect with",
+)
+
+var availableWhenDonor = flag.Bool(
+	"availWhenDonor",
+	false,
+	"Specifies if the healthcheck allows availability when in donor state",
+)
+
+var availableWhenReadOnly = flag.Bool(
+	"availWhenReadOnly",
+	false,
+	"Specifies if the healthcheck allows availability when in read only mode",
+)
+
+var healthchecker *healthcheck.Healthchecker
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	result,msg := healthchecker.Check()
+	if (result) {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}
+	fmt.Fprintf(w, "Galera Cluster Node is %s", msg)
+}
+
+func main() {
+	flag.Parse()
+
+	db, _ := sql.Open("mysql", fmt.Sprintf("%s:%s@/", *mysqlUser, *mysqlPassword))
+	config := healthcheck.HealthcheckerConfig{
+		*availableWhenDonor,
+		*availableWhenReadOnly,
+	}
+
+	healthchecker = healthcheck.New(db, config)
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(fmt.Sprintf(":%d", *serverPort), nil)
+}
