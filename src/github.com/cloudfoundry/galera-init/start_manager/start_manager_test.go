d23e345ad2caa95c0cc9328c06c4bcde60769dcf
Lyle Franklin and Raina Masand <lfranklin@pivotal.io>
2015-02-10 14:53:26 -0800

    More test refactors
    - Consolidate some BeforeEach repetition into higher level of test
    organization
    - Rearrange grouping of tests
    [#87476258]

--- src/github.com/cloudfoundry/galera-init/start_manager/start_manager_test.go
+++ src/github.com/cloudfoundry/galera-init/start_manager/start_manager_test.go
@@ -122,63 +122,84 @@ var _ = Describe("StartManager", func() {
 		})
 	})
 
-	Context("When determining whether upgrade is required with NeedsUpgrade exits with an error", func() {
-		BeforeEach(func() {
-			mgr = createManager(0, 3)
+	Describe("Upgrade", func() {
+		Context("When determining whether upgrade is required with NeedsUpgrade exits with an error", func() {
+			BeforeEach(func() {
+				mgr = createManager(0, 3)
 
-			fakeUpgrader.NeedsUpgradeReturns(false, errors.New("Error determining whether upgrade is required"))
+				fakeUpgrader.NeedsUpgradeReturns(false, errors.New("Error determining whether upgrade is required"))
+			})
+
+			It("forwards the error", func() {
+				err := mgr.Execute()
+				Expect(err).To(HaveOccurred())
+			})
 		})
 
-		It("forwards the error", func() {
-			err := mgr.Execute()
-			Expect(err).To(HaveOccurred())
+		Context("When upgrade is required", func() {
+			Context("And performing the upgrade exits with an error", func() {
+
+				BeforeEach(func() {
+					mgr = createManager(0, 3)
+
+					fakeUpgrader.NeedsUpgradeReturns(true, nil)
+					fakeUpgrader.UpgradeReturns(errors.New("Error while performing upgrade"))
+				})
+
+				It("forwards the error", func() {
+					err := mgr.Execute()
+					Expect(err).To(HaveOccurred())
+				})
+			})
 		})
 	})
 
-	Context("When there's an error seeding the databases", func() {
-		BeforeEach(func() {
-			stubPgrepCheck(fakeOs)
-
-			mgr = createManager(0, 1)
-		})
-
-		Context("And the total attempts at seeding the database is less than maxDatabaseSeedTries", func() {
+	Describe("SeedDatabases", func() {
+		Context("When there's an error seeding the databases", func() {
 			BeforeEach(func() {
-				numTries := 0
-				fakeOs.RunCommandStub = func(arg1 string, arg2 ...string) (string, error) {
-					numTries++
-					if numTries < maxDatabaseSeedTries {
-						return "", errors.New("seeding databases failed")
-					} else {
-						return "succeeded", nil
+				stubPgrepCheck(fakeOs)
+
+				mgr = createManager(0, 1)
+			})
+
+			Context("And the total attempts at seeding the database is less than maxDatabaseSeedTries", func() {
+				BeforeEach(func() {
+					numTries := 0
+					fakeOs.RunCommandStub = func(arg1 string, arg2 ...string) (string, error) {
+						numTries++
+						if numTries < maxDatabaseSeedTries {
+							return "", errors.New("seeding databases failed")
+						} else {
+							return "succeeded", nil
+						}
 					}
-				}
+				})
+
+				It("waits and attempts to retry to seed the database", func() {
+					err := mgr.Execute()
+					Expect(err).ToNot(HaveOccurred())
+					ensureSeedDatabases()
+				})
 			})
 
-			It("waits and attempts to retry to seed the database", func() {
-				err := mgr.Execute()
-				Expect(err).ToNot(HaveOccurred())
-				ensureSeedDatabases()
-			})
-		})
+			Context("And the total attempts at seeding the database is greater than or equal to maxDatabaseSeedTries", func() {
+				var numTries int
+				BeforeEach(func() {
+					numTries = 0
+					fakeOs.RunCommandStub = func(arg1 string, arg2 ...string) (string, error) {
+						numTries++
+						return "", errors.New("seeding databases failed")
+					}
+				})
 
-		Context("And the total attempts at seeding the database is greater than or equal to maxDatabaseSeedTries", func() {
-			var numTries int
-			BeforeEach(func() {
-				numTries = 0
-				fakeOs.RunCommandStub = func(arg1 string, arg2 ...string) (string, error) {
-					numTries++
-					return "", errors.New("seeding databases failed")
-				}
-			})
-
-			It("exits and stops mysql (so the deploy fails) and does not write to the state file", func() {
-				err := mgr.Execute()
-				Expect(err).To(HaveOccurred())
-				Expect(numTries).To(Equal(maxDatabaseSeedTries))
-				Expect(fakeOs.SleepCallCount()).To(Equal(maxDatabaseSeedTries))
-				ensureStop()
-				ensureNoWriteToStateFile()
+				It("exits and stops mysql (so the deploy fails) and does not write to the state file", func() {
+					err := mgr.Execute()
+					Expect(err).To(HaveOccurred())
+					Expect(numTries).To(Equal(maxDatabaseSeedTries))
+					Expect(fakeOs.SleepCallCount()).To(Equal(maxDatabaseSeedTries))
+					ensureStop()
+					ensureNoWriteToStateFile()
+				})
 			})
 		})
 	})
@@ -219,48 +240,55 @@ var _ = Describe("StartManager", func() {
 		})
 	})
 
-	Context("When starting in multi-node deployment on a node > 0", func() {
+	Context("When starting in multi-node deployment", func() {
 
 		BeforeEach(func() {
 			stubPgrepCheck(fakeOs)
-
-			mgr = createManager(1, 3)
 		})
 
-		It("joins cluster, seeds databases, and writes '"+CLUSTERED+"' to file", func() {
-			err := mgr.Execute()
-			Expect(err).ToNot(HaveOccurred())
-			ensureJoin()
-			ensureSeedDatabases()
-		})
-	})
-
-	Context("When starting in multi-node deployment on node 0", func() {
-
-		BeforeEach(func() {
-			stubPgrepCheck(fakeOs)
-			fakeClusterHealthChecker.HealthyClusterReturns(false)
-
-			mgr = createManager(0, 3)
-		})
-
-		Context("And it's an initial deploy", func() {
+		Context("And it's an initial deploy, so there's no state file", func() {
 			BeforeEach(func() {
 				fakeOs.FileExistsReturns(false)
 			})
+			Context("And jobIndex == 0", func() {
+				BeforeEach(func() {
+					mgr = createManager(0, 3)
+					fakeClusterHealthChecker.HealthyClusterReturns(false)
+				})
 
-			It("bootstraps, seeds databases and writes "+CLUSTERED+" to file", func() {
-				err := mgr.Execute()
-				Expect(err).ToNot(HaveOccurred())
-				ensureBootstrapWithStateFileContents(CLUSTERED)
-				ensureSeedDatabases()
+				It("bootstraps, seeds databases and writes "+CLUSTERED+" to file", func() {
+					err := mgr.Execute()
+					Expect(err).ToNot(HaveOccurred())
+					ensureBootstrapWithStateFileContents(CLUSTERED)
+					ensureSeedDatabases()
+				})
+			})
+
+			Context("And jobIndex > 0", func() {
+
+				BeforeEach(func() {
+					stubPgrepCheck(fakeOs)
+
+					mgr = createManager(1, 3)
+				})
+
+				It("joins cluster, seeds databases, and writes '"+CLUSTERED+"' to file", func() {
+					err := mgr.Execute()
+					Expect(err).ToNot(HaveOccurred())
+					ensureJoin()
+					ensureSeedDatabases()
+				})
 			})
 		})
 
 		Context("When state file is present", func() {
-			Context("And contains extra whitespace characters as well as '"+CLUSTERED+"'", func() {
+			BeforeEach(func() {
+				mgr = createManager(0, 3)
+				fakeOs.FileExistsReturns(true)
+			})
+
+			Context("And contains extra whitespace characters as well as a valid state", func() {
 				BeforeEach(func() {
-					fakeOs.FileExistsReturns(true)
 					fakeOs.ReadFileReturns(fmt.Sprintf("\n\n     %s \n", CLUSTERED), nil)
 				})
 
@@ -271,9 +299,9 @@ var _ = Describe("StartManager", func() {
 					ensureSeedDatabases()
 				})
 			})
+
 			Context("And reads '"+CLUSTERED+"'", func() {
 				BeforeEach(func() {
-					fakeOs.FileExistsReturns(true)
 					fakeOs.ReadFileReturns(CLUSTERED, nil)
 				})
 
@@ -287,37 +315,28 @@ var _ = Describe("StartManager", func() {
 
 			Context("And reads '"+NEEDS_BOOTSTRAP+"'", func() {
 				BeforeEach(func() {
-					fakeOs.FileExistsReturns(true)
 					fakeOs.ReadFileReturns(NEEDS_BOOTSTRAP, nil)
 				})
 
-				Context("And jobIndex == 0", func() {
+				It("joins cluster, seeds databases, and writes '"+CLUSTERED+"' to file", func() {
+					err := mgr.Execute()
+					Expect(err).NotTo(HaveOccurred())
+					ensureBootstrapWithStateFileContents(CLUSTERED)
+					ensureSeedDatabases()
+				})
+
+				Context("And one or more other nodes is reachable", func() {
 					BeforeEach(func() {
-						stubPgrepCheck(fakeOs)
+						fakeClusterHealthChecker.HealthyClusterReturns(true)
 
 						mgr = createManager(0, 3)
 					})
 
-					It("joins cluster, seeds databases, and writes '"+CLUSTERED+"' to file", func() {
+					It("joins the cluster and seeds databases", func() {
 						err := mgr.Execute()
-						Expect(err).NotTo(HaveOccurred())
-						ensureBootstrapWithStateFileContents(CLUSTERED)
+						Expect(err).ToNot(HaveOccurred())
+						ensureJoin()
 						ensureSeedDatabases()
-					})
-
-					Context("And one or more other nodes is reachable", func() {
-						BeforeEach(func() {
-							fakeClusterHealthChecker.HealthyClusterReturns(true)
-
-							mgr = createManager(0, 3)
-						})
-
-						It("joins the cluster and seeds databases", func() {
-							err := mgr.Execute()
-							Expect(err).ToNot(HaveOccurred())
-							ensureJoin()
-							ensureSeedDatabases()
-						})
 					})
 				})
 
@@ -339,7 +358,6 @@ var _ = Describe("StartManager", func() {
 
 			Context("And contains an invalid state", func() {
 				BeforeEach(func() {
-					fakeOs.FileExistsReturns(true)
 					fakeOs.ReadFileReturns("INVALID_STATE", nil)
 				})
 
@@ -392,22 +410,4 @@ var _ = Describe("StartManager", func() {
 		})
 	})
 
-	Context("When upgrade is required", func() {
-		Context("And performing the upgrade exits with an error", func() {
-
-			BeforeEach(func() {
-				mgr = createManager(0, 3)
-
-				fakeUpgrader.NeedsUpgradeReturns(true, nil)
-				fakeUpgrader.UpgradeReturns(errors.New("Error while performing upgrade"))
-			})
-
-			It("forwards the error", func() {
-				err := mgr.Execute()
-				Expect(err).To(HaveOccurred())
-			})
-
-		})
-
-	})
 })
