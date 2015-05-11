325c13173d3f74d3bd2d586fd5092671aab8a1a9
Lyle Franklin and Shash Reddy <lfranklin@pivotal.io>
2015-05-11 11:29:38 -0700

    Fixed typo in main compilation test
    [#91818768]

--- src/github.com/cloudfoundry/galera-init/main_test.go
+++ src/github.com/cloudfoundry/galera-init/main_test.go
@@ -9,7 +9,7 @@ import (
 var _ = Describe("Executable", func() {
 
 	It("compiles the binary without errors", func() {
-		_, err := gexec.Build("github.com/cloudfoundry-incubator/switchboard/")
+		_, err := gexec.Build("github.com/cloudfoundry/mariadb_ctrl")
 		gexec.CleanupBuildArtifacts()
 		Expect(err).NotTo(HaveOccurred())
 	})
