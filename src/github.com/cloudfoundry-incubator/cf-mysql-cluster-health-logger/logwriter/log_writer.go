540999f39b015d037e7a41774b8ee9c89bcd59e3
Andrew Crump <acrump@pivotal.io>
2016-05-05 17:00:38 -0700

    Create main for cluster health logger [#118415807]
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- src/github.com/cloudfoundry-incubator/cf-mysql-cluster-health-logger/logwriter/log_writer.go
+++ src/github.com/cloudfoundry-incubator/cf-mysql-cluster-health-logger/logwriter/log_writer.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strings"
 	"fmt"
+	"log"
 )
 
 type LogWriter interface {
@@ -27,8 +28,22 @@ func (lw *logWriter) Write(ts string) {
 	var statusColumnNames [9]string
 	var statusColumnValues [9]string
 
-	statusQuery := "SHOW STATUS WHERE Variable_name IN ('wsrep_ready','wsrep_cluster_conf_id','wsrep_cluster_status','wsrep_connected','wsrep_local_state_comment','wsrep_local_recv_queue_avg','wsrep_flow_control_paused','wsrep_cert_deps_distance','wsrep_local_send_queue_avg')"
-	status, _ := lw.db.Query(statusQuery)
+	statusQuery := `SHOW STATUS WHERE Variable_name IN (
+		'wsrep_ready',
+		'wsrep_cluster_conf_id',
+		'wsrep_cluster_status',
+		'wsrep_connected',
+		'wsrep_local_state_comment',
+		'wsrep_local_recv_queue_avg',
+		'wsrep_flow_control_paused',
+		'wsrep_cert_deps_distance',
+		'wsrep_local_send_queue_avg'
+		)`
+	status, err := lw.db.Query(statusQuery)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	index := 0
 	defer status.Close()
@@ -40,22 +55,22 @@ func (lw *logWriter) Write(ts string) {
 		statusColumnValues[index] = varValue
 		index++
 	}
-	_, err := os.Stat(lw.logPath)
+	_, err = os.Stat(lw.logPath)
 	writeHeaders := false
 	if err != nil {
 		writeHeaders = true
 	}
 
 	f, _ := os.OpenFile(lw.logPath, os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0666)
+	defer f.Close()
 
 	columnNamesStr := strings.Join(statusColumnNames[0:9], ",")
 	columnValuesStr := strings.Join(statusColumnValues[0:9], ",")
 
-	if (writeHeaders) {
+	if writeHeaders {
 		f.WriteString(fmt.Sprintf("%s,%s","timestamp", columnNamesStr))
 		f.WriteString("\n")
 	}
 	f.WriteString(fmt.Sprintf("%s,%s",ts, columnValuesStr))
 	f.WriteString("\n")
-	f.Close()
 }
