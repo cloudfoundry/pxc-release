d0f35dd5ce43105ecf112f88f0fb1ef95f82e68c
Difan Zhao <dzhao@pivotal.io>
2017-04-13 14:31:42 -0700

    refactor start to be consistent with pre-start
    [#142706725]
    Signed-off-by: Andrew Garner <agarner@pivotal.io>

--- src/github.com/cloudfoundry/galera-init/cmd/start/main.go
+++ src/github.com/cloudfoundry/galera-init/cmd/start/main.go
@@ -4,8 +4,9 @@ import (
 	"io/ioutil"
 	"os"
 
-	"code.cloudfoundry.org/lager"
 	"fmt"
+
+	"code.cloudfoundry.org/lager"
 	"github.com/cloudfoundry/mariadb_ctrl/cluster_health_checker"
 	"github.com/cloudfoundry/mariadb_ctrl/config"
 	"github.com/cloudfoundry/mariadb_ctrl/mariadb_helper"
@@ -29,11 +30,12 @@ func main() {
 		return
 	}
 
-	err = managerSetup(cfg)
+	startManager := managerSetup(cfg)
+	err = startManager.Execute()
 
 	if err != nil {
 		cfg.Logger.Info(err.Error())
-		panic("manager setup failed")
+		panic("manager start failed")
 	}
 	err = writePidFile(cfg)
 	if err != nil {
@@ -46,19 +48,6 @@ func main() {
 
 	cfg.Logger.Info("mariadb_ctrl started")
 
-	//
-	//err = deletePidFile(cfg)
-	//if err != nil {
-	//	cfg.Logger.Error("Error deleting pidfile", err, lager.Data{
-	//		"pidfile": cfg.PidFile,
-	//	})
-	//}
-	//
-	//if processErr != nil {
-	//	cfg.Logger.Fatal("Error starting mariadb_ctrl", processErr)
-	//}
-	//
-	//cfg.Logger.Info("Process exited without error.")
 }
 
 func writePidFile(cfg *config.Config) error {
@@ -80,7 +69,7 @@ func deletePidFile(cfg *config.Config) error {
 	return os.Remove(cfg.PidFile)
 }
 
-func managerSetup(cfg *config.Config) error {
+func managerSetup(cfg *config.Config) start_manager.StartManager {
 	OsHelper := os_helper.NewImpl()
 
 	DBHelper := mariadb_helper.NewMariaDBHelper(
@@ -120,21 +109,5 @@ func managerSetup(cfg *config.Config) error {
 		ClusterHealthChecker,
 	)
 
-	err := NodeStartManager.Execute()
-	if err != nil {
-		cfg.Logger.Info("execute error")
-		return err
-	}
-
-	//cmd, err := NodeStarter.GetMysqlCmd()
-	//if err != nil {
-	//	cfg.Logger.Info("GetMysqlCmderror")
-	//	return -1, err
-	//}
-	return nil
-	// runner := node_runner.NewRunner(NodeStartManager, cfg.Logger)
-
-	// sigRunner := sigmon.New(runner, os.Kill)
-
-	// return sigRunner
+	return NodeStartManager
 }
