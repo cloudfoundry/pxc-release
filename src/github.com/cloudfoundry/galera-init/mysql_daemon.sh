7ddbacc7927010ce3a0d6027f398ed5215b8bbab
Adrian Zankich <azankich@pivotal.io>
2017-06-01 16:16:14 -0700

    All starting and stopping use the default pidfile
    [#145530067](https://www.pivotaltracker.com/story/show/145530067)
    
    Signed-off-by: Sam Serrano <sserrano@pivotal.io>

--- src/github.com/cloudfoundry/galera-init/mysql_daemon.sh
+++ src/github.com/cloudfoundry/galera-init/mysql_daemon.sh
@@ -4,34 +4,15 @@ set -e
 
 mode=$1
 
-STANDALONE_PID_FILE=/tmp/tmp-mysql.pid
-
 case "$mode" in
   'start')
       echo "Starting MySQL"
       exec /var/vcap/packages/mariadb/bin/mysqld_safe
       ;;
 
-  'stop')
+  'stop' | 'stop-stand-alone')
       echo "Stopping the cluster"
       /var/vcap/packages/mariadb/support-files/mysql.server stop
-      ;;
-
-  # This was mostly taken from /var/vcap/packages/mariadb/support-files/mysql.server
-  # It cannot handle the fact the pid-file is in a different location than defined in my.cnf
-  # We don't wait for the process to stop, because the waiting/polling takes places at a higher level.
-  'stop-stand-alone')
-      echo "Stopping the standalone node"
-      mysqld_pid=`cat "$STANDALONE_PID_FILE"`
-
-      if (kill -0 $mysqld_pid 2>/dev/null)
-      then
-        echo $echo_n "Shutting down node"
-        kill $mysqld_pid
-      else
-        echo "Server process #$mysqld_pid is not running!"
-        rm "$STANDALONE_PID_FILE"
-      fi
       ;;
 
   'bootstrap')
@@ -43,7 +24,7 @@ case "$mode" in
 
   'stand-alone')
       echo "Starting the node in stand-alone mode"
-      /var/vcap/packages/mariadb/bin/mysqld_safe --wsrep-on=OFF --wsrep-desync=ON --wsrep-OSU-method=RSU --wsrep-provider='none' --pid-file=$STANDALONE_PID_FILE &
+      /var/vcap/packages/mariadb/bin/mysqld_safe --wsrep-on=OFF --wsrep-desync=ON --wsrep-OSU-method=RSU --wsrep-provider='none' &
       ;;
 
   'status')
