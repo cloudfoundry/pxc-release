a3da4943fd414bff70c4b1583e23701095eed422
Joseph Palermo <jpalermo@pivotal.io>
2018-04-12 11:20:34 -0700

    Sigar gives disk sizes in KBytes; Adjust migration accordingly [#156407982 #156291976]
    Signed-off-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- src/migrate-to-pxc/disk/disk.go
+++ src/migrate-to-pxc/disk/disk.go
@@ -5,7 +5,6 @@ import (
 	"github.com/cloudfoundry/gosigar"
 )
 
-
 //go:generate counterfeiter . Sigar
 type Sigar interface {
 	GetFileSystemUsage(string) (sigar.FileSystemUsage, error)
@@ -18,10 +17,11 @@ func RoomToMigrate(systemInfoGatherer Sigar) error {
 		return err
 	}
 
-	emptyDBSizeBytes := uint64(2500000000) // Approximate size of an empty Percona installation
-	usedBytes := fileSystemUsage.Used
-	totalBytes := fileSystemUsage.Total
-	if 100*(usedBytes-emptyDBSizeBytes)/totalBytes > 45 {
+	emptyDBSizeKBytes := uint64(2500000) // Approximate size of an empty Percona installation
+	usedKBytes := fileSystemUsage.Used
+	totalKBytes := fileSystemUsage.Total
+
+	if 100*(usedKBytes-emptyDBSizeKBytes)/totalKBytes > 45 {
 		return errors.New("Cannot continue, insufficient disk space to complete migration")
 	}
 	return nil
