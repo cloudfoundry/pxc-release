5ec7df70d81f917241b79819f8cb0ad9620b9192
Joseph Palermo <jpalermo@pivotal.io>
2018-05-10 15:33:05 -0700

    Prevent drain script from failing if release is disabled [#157496349]

--- jobs/mysql-clustered/templates/drain.sh
+++ jobs/mysql-clustered/templates/drain.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
+<% if p('pxc_enabled') == true %>
 /var/vcap/packages/pxc/bin/mysqladmin --defaults-file=/var/vcap/jobs/mysql-clustered/config/mylogin.cnf shutdown > /dev/null
 return_code=$?
 echo 0
 exit ${return_code}
+<% else %>
+echo 0
+exit 0
+<% end %>
