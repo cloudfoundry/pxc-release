337cf599eb0c91ea0c51ddb3c8d5201609b9c5a5
Kai Xiang <kxiang@pivotal.io>
2019-05-07 12:02:12 -0700

    Test singlenode functionality in src/specs/integration/singlenode
    Adds a test to check that, on startup, there is an empty GTID
    transactions history.
    
    [#165755792]
    
    Signed-off-by: Chris Piraino <cpiraino@pivotal.io>

---
+++ src/specs/integration/singlenode/singlenode_test.go
@@ -0,0 +1,30 @@
+package singlenode_test
+
+import (
+	helpers "specs/test_helpers"
+
+	_ "github.com/go-sql-driver/mysql"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CF PXC Single Node", func() {
+	AfterEach(func() {
+		_, err := mysqlConn.Query("RESET MASTER")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("has an empty GTID transaction history on startup", func() {
+		var queryResultString string
+		query := "select @@GLOBAL.gtid_executed;"
+		row := mysqlConn.QueryRow(query)
+		Expect(row.Scan(&queryResultString)).To(Succeed())
+		Expect(queryResultString).To(BeEmpty())
+
+		helpers.DbSetup(mysqlConn, "binarylogs")
+
+		row = mysqlConn.QueryRow(query)
+		Expect(row.Scan(&queryResultString)).To(Succeed())
+		Expect(queryResultString).ToNot(BeEmpty())
+	})
+})
