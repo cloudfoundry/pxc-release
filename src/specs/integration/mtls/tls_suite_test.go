baf4c0c60f69688d9e109e723cc86d1c03c59459
Andrew Garner <agarner@pivotal.io>
2019-05-10 16:38:14 -0700

    Update vendored dependencies for mtls test
    Signed-off-by: James Palmer <jpalmer@pivotal.io>

---
+++ src/specs/integration/mtls/tls_suite_test.go
@@ -0,0 +1,48 @@
+package tls_test
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	helpers "specs/test_helpers"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestTls(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Tls Suite")
+}
+
+var (
+	mysqlConn *sql.DB
+)
+
+var _ = BeforeSuite(func() {
+	requiredEnvs := []string{
+		"BOSH_ENVIRONMENT",
+		"BOSH_CA_CERT",
+		"BOSH_CLIENT",
+		"BOSH_CLIENT_SECRET",
+		"BOSH_DEPLOYMENT",
+		"CREDHUB_SERVER",
+		"CREDHUB_CLIENT",
+		"CREDHUB_SECRET",
+	}
+	helpers.CheckForRequiredEnvVars(requiredEnvs)
+
+	helpers.SetupBoshDeployment()
+
+	if os.Getenv("BOSH_ALL_PROXY") != "" {
+		helpers.SetupSocks5Proxy()
+	}
+
+	mysqlUsername := "root"
+	mysqlPassword, err := helpers.GetMySQLAdminPassword()
+	Expect(err).NotTo(HaveOccurred())
+	firstProxy, err := helpers.FirstProxyHost(helpers.BoshDeployment)
+	Expect(err).NotTo(HaveOccurred())
+	mysqlConn = helpers.DbConnWithUser(mysqlUsername, mysqlPassword, firstProxy)
+})
