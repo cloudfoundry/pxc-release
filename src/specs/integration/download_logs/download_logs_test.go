f1a08518b8e413d6375e4c5604015078a037b3d1
Alvaro Perez-Shirley <aperezshirley@pivotal.io>
2019-10-28 14:28:51 -0700

    Add test for download-logs fetching INOODB ENGINE STATUS
    [#169072466]
    
    Signed-off-by: Andrew Garner <agarner@pivotal.io>

---
+++ src/specs/integration/download_logs/download_logs_test.go
@@ -0,0 +1,56 @@
+package download_logs_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("DownloadLogs", func() {
+	var (
+		logsDir string
+	)
+	BeforeEach(func() {
+		var err error
+		logsDir, err = ioutil.TempDir(os.TempDir(), "download_logs_")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		os.RemoveAll(logsDir)
+	})
+
+	It("fetches SHOW ENGINE INNODB STATUS output", func() {
+		downloadLogsCmd := exec.Command("./download-logs", "-o", logsDir)
+		downloadLogsCmd.Env = append(os.Environ(), "DOWNLOAD_LOGS_GPG_PASSPHRASE_FROM_STDIN=true")
+		downloadLogsCmd.Dir = `../../../../scripts/`
+		downloadLogsCmd.Stdin = bytes.NewBufferString("some-passphrase")
+
+		session, err := gexec.Start(downloadLogsCmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(session, "10m", "1s").Should(gexec.Exit(0))
+
+		innodbStatusOutput := gbytes.NewBuffer()
+		gpgCmd := fmt.Sprintf(`gpg -d --batch --passphrase=some-passphrase < %s/*-mysql-logs.tar.gz.gpg `+
+			`| gtar -Ozxv --wildcards "*/innodb_status.out"`, logsDir)
+		decryptCmd := exec.Command("bash", "-c", gpgCmd)
+
+		stdout := io.MultiWriter(GinkgoWriter, innodbStatusOutput)
+
+		session, err = gexec.Start(decryptCmd, stdout, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(session, "10m", "1s").Should(gexec.Exit(0))
+		Expect(innodbStatusOutput).To(gbytes.Say(`(?m)^END OF INNODB MONITOR OUTPUT\s*$`))
+	})
+})
