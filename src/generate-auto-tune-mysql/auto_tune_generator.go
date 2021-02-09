8c365148fb29c4efbd57d4eb3274774e76bd9d26
Colin Shield <cshield@vmware.com>
2021-02-08 16:35:22 -0800

    Add property to tune binlog space limit
    [#176726998](https://www.pivotaltracker.com/story/show/176726998)
    
    Co-authored-by: Colin Shield <cshield@vmware.com>
    Co-authored-by: Shaan Sapra <shsapra@vmware.com>

--- src/generate-auto-tune-mysql/auto_tune_generator.go
+++ src/generate-auto-tune-mysql/auto_tune_generator.go
@@ -3,17 +3,44 @@ package main
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/pkg/errors"
 )
 
-func Generate(totalMem uint64, targetPercentage float64, writer io.Writer) error {
+const binlogBlockSize = 4 * 1024
 
-	bufferPoolSize := float64(totalMem) * targetPercentage / 100.0
-	_, err := writer.Write([]byte(fmt.Sprintf(`
-[mysqld]
-innodb_buffer_pool_size = %d
-`, uint64(bufferPoolSize))))
+func Generate(totalMem, totalDiskinKB uint64, targetPercentageofMem, targetPercentageofDisk float64, writer io.Writer) error {
 
+	bufferPoolSize := float64(totalMem) * targetPercentageofMem / 100.0
+
+	params := []string{}
+	params = append(params, fmt.Sprintf("innodb_buffer_pool_size = %d", uint64(bufferPoolSize)))
+
+	if targetPercentageofDisk != 0.0 {
+		totalDisk := totalDiskinKB * 1024
+		binLogSpaceLimit := float64(totalDisk) * targetPercentageofDisk / 100.0
+		params = append(params, fmt.Sprintf("binlog_space_limit = %d", uint64(binLogSpaceLimit)))
+
+		maxBinlogSize := min(int64(binLogSpaceLimit / 3), 1024*1024*1024)
+		maxBinlogSize = (maxBinlogSize/binlogBlockSize)*binlogBlockSize
+
+		params = append(params, fmt.Sprintf("max_binlog_size = %d", uint64(maxBinlogSize)))
+	}
+
+	config := "\n[mysqld]\n" + strings.Join(params, "\n") + "\n"
+	_, err := writer.Write([]byte(config))
 	return errors.Wrap(err, "failed to emit mysql configuration")
+}
+
+func min(a int64, b ...int64) int64 {
+	m := a
+
+	for _, i := range b {
+		if i < m {
+			m = i
+		}
+	}
+
+	return m
 }
--- src/generate-auto-tune-mysql/auto_tune_generator_test.go
+++ src/generate-auto-tune-mysql/auto_tune_generator_test.go
@@ -1,54 +1,105 @@
 package main_test
 
 import (
+	"bytes"
 	"errors"
-
 	. "github.com/cloudfoundry/generate-auto-tune-mysql"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"bytes"
 )
 
-var includeFileAt42 = `
+var sampleConfig1 = `
+[mysqld]
+innodb_buffer_pool_size = 84
+binlog_space_limit = 214748364
+max_binlog_size = 71581696
+`
+
+var sampleConfig2 = `
+[mysqld]
+innodb_buffer_pool_size = 6
+binlog_space_limit = 2149135626
+max_binlog_size = 716378112
+`
+
+var sampleConfig3 = `
+[mysqld]
+innodb_buffer_pool_size = 84
+binlog_space_limit = 5368709120
+max_binlog_size = 1073741824
+`
+
+var sampleConfig4 = `
 [mysqld]
 innodb_buffer_pool_size = 84
 `
 
-var includeFileAt7 = `
-[mysqld]
-innodb_buffer_pool_size = 6
-`
-
 var _ = Describe("AutoTuneGenerator", func() {
 	Describe("Generate", func() {
-		It("writes file with correct innodb buffer size", func() {
-			totalMem := uint64(200)
-			targetPercentage := float64(42)
-			writer := &bytes.Buffer{}
+		var (
+			totalMem               uint64
+			totalDiskinKB          uint64
+			targetPercentageofMem  float64
+			targetPercentageofDisk float64
+		)
 
-			Expect(Generate(totalMem, targetPercentage, writer)).To(Succeed())
-
-			Expect(writer.String()).To(Equal(includeFileAt42))
+		BeforeEach(func() {
+			totalMem = uint64(200)
+			totalDiskinKB = uint64(2 * 1024 * 1024)
+			targetPercentageofMem = float64(42)
+			targetPercentageofDisk = float64(10)
 		})
 
-		It("floors floating numbers to whole integers of bytes", func() {
-			totalMem := uint64(10)
-			targetPercentage := float64(66)
+		It("writes file with correct parameters", func() {
 			writer := &bytes.Buffer{}
+			Expect(Generate(totalMem, totalDiskinKB, targetPercentageofMem, targetPercentageofDisk, writer)).To(Succeed())
+			Expect(writer.String()).To(Equal(sampleConfig1))
+		})
 
-			Expect(Generate(totalMem, targetPercentage, writer)).To(Succeed())
+		Context("when the calculations result in floating numbers", func() {
+			BeforeEach(func() {
+				totalMem = uint64(10)
+				totalDiskinKB = uint64(12345678)
+				targetPercentageofMem = float64(66)
+				targetPercentageofDisk = float64(17)
+			})
 
-			Expect(writer.String()).To(Equal(includeFileAt7))
+			It("floors floating numbers to whole integers of bytes", func() {
+				writer := &bytes.Buffer{}
+				Expect(Generate(totalMem, totalDiskinKB, targetPercentageofMem, targetPercentageofDisk, writer)).To(Succeed())
+				Expect(writer.String()).To(Equal(sampleConfig2))
+			})
+		})
+
+		Context("when using binlog space limit is more than 3GB", func() {
+			BeforeEach(func() {
+				totalDiskinKB = uint64(10 * 1024 * 1024)
+				targetPercentageofDisk = float64(50)
+			})
+
+			It("sets the maximum binlog file size to 1GB", func() {
+				writer := &bytes.Buffer{}
+				Expect(Generate(totalMem, totalDiskinKB, targetPercentageofMem, targetPercentageofDisk, writer)).To(Succeed())
+				Expect(writer.String()).To(Equal(sampleConfig3))
+			})
+		})
+
+		Context("when using the default target percentage of disk of 0", func() {
+			BeforeEach(func() {
+				targetPercentageofDisk = 0.0
+			})
+
+			It("does not set binlog parameters", func() {
+				writer := &bytes.Buffer{}
+				Expect(Generate(totalMem, totalDiskinKB, targetPercentageofMem, targetPercentageofDisk, writer)).To(Succeed())
+				Expect(writer.String()).To(Equal(sampleConfig4))
+			})
 		})
 
 		Context("when writing the config file fails", func() {
 			It("returns an error", func() {
-				totalMem := uint64(10)
-				targetPercentage := float64(66)
 				writer := FailingWriter{}
-
-				err := Generate(totalMem, targetPercentage, writer)
+				err := Generate(totalMem, totalDiskinKB, targetPercentageofMem, targetPercentageofDisk, writer)
 				Expect(err).To(MatchError(`failed to emit mysql configuration: write failed`))
 			})
 		})
