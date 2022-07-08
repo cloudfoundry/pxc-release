7f7625d711a9077ff2a8fbe873b28ac8ce0cb1eb
Kyle Ong <kyleo@vmware.com>
2022-07-08 11:19:40 -0700

    no_remote_access_test: Add disableRemoteAdminAccess
    - Test is now responsible for disabling remote admin access
    
    [#181287853](https://www.pivotaltracker.com/story/show/181287853)
    
    Co-authored-by: Kyle Ong <kyleo@vmware.com>
    Co-authored-by: Andrew Garner <garnera@vmware.com>

--- src/specs/integration/no_remote_access/no_remote_access_test.go
+++ src/specs/integration/no_remote_access/no_remote_access_test.go
@@ -1,12 +1,36 @@
 package no_remote_access_test
 
 import (
+	"os"
+	"os/exec"
+
 	_ "github.com/go-sql-driver/mysql"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
+func disableRemoteAdminAccess() {
+	tmp, err := os.CreateTemp("", os.Getenv("BOSH_DEPLOYMENT") + "bosh-manifest.yml")
+	Expect(err).NotTo(HaveOccurred())
+
+	defer os.Remove(tmp.Name())
+
+	cmd := exec.Command("bosh", "manifest")
+	cmd.Stdout = tmp
+	cmd.Stderr = GinkgoWriter
+	Expect(cmd.Run()).To(Succeed())
+
+	cmd = exec.Command("bosh", "deploy", tmp.Name(),
+		"-o", "../../../../operations/disable-remote-admin-access.yml", "-n", "--tty")
+	cmd.Stderr = GinkgoWriter
+	cmd.Stdout = GinkgoWriter
+	Expect(cmd.Run()).To(Succeed())
+}
+
 var _ = Describe("CF PXC No Remote Admin Access", func() {
+	BeforeEach(func() {
+		disableRemoteAdminAccess()
+	})
 	It("does not allow access to mysql from anywhere besides localhost", func() {
 		query := "show variables"
 		_, err := mysqlConn.Query(query)
