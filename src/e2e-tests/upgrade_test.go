30a55129e74fee79a278c317bbb9cd9b5e1ce6e2
Andrew Garner <garnera@vmware.com>
2022-09-08 13:42:25 -0500

    e2e-tests: upgrade test validates an upgrade from a crashed pxc-5.7 instance
    - Upgrade now starts from a one-node pxc-5.7 deployment
    - pxc-5.7 deployment is now written to and mysqld terminated with
      SIGKILL to simulate an unclean shutdown
    
    This is currently a failing tst as the upgrade to pxc-8.0 will fail with
    the error message:
    
    > [ERROR] [MY-012526] [InnoDB] Upgrade after a crash is not supported.
    
    [#183085049](https://www.pivotaltracker.com/story/show/183085049)
    
    Co-authored-by: Andrew Garner <garnera@vmware.com>
    Co-authored-by: Kyle Ong <kyleo@vmware.com>

--- src/e2e-tests/upgrade_test.go
+++ src/e2e-tests/upgrade_test.go
@@ -1,6 +1,8 @@
 package e2e_tests
 
 import (
+	"database/sql"
+
 	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -27,6 +29,28 @@ var _ = Describe("Upgrade", Label("upgrade"), func() {
 
 		Expect(bosh.RunErrand(deploymentName, "smoke-tests", "mysql/first")).To(Succeed())
 
+		By("intentionally crashing the first node", func() {
+			proxyIPs, err := bosh.InstanceIPs(deploymentName, bosh.MatchByInstanceGroup("proxy"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(proxyIPs).ToNot(BeEmpty())
+
+			db, err := sql.Open("mysql", "test-user:integration-tests@tcp("+proxyIPs[0]+")/?tls=skip-verify")
+			Expect(err).NotTo(HaveOccurred())
+			defer db.Close()
+
+			Expect(db.Exec(`CREATE TABLE IF NOT EXISTS crash_upgrade_test.t1 (id int primary key auto_increment, data varchar(40))`)).Error().NotTo(HaveOccurred())
+
+			Consistently(func() error {
+				_, err := db.Exec(`INSERT INTO crash_upgrade_test.t1 (data) VALUES (SHA1(RAND()))`)
+				return err
+			}, "15s").Should(Succeed())
+
+			Expect(bosh.RemoteCommand(deploymentName,
+				"mysql/0",
+				`sudo monit unmonitor galera-init && sudo killall -9 mysqld`),
+			).Error().NotTo(HaveOccurred())
+		})
+
 		By("forcing credhub to rotate the galera-agent database credential")
 		Expect(credhub.Regenerate("/" + deploymentName + "/cf_mysql_mysql_galera_healthcheck_db_password")).
 			To(Succeed())
@@ -43,9 +67,16 @@ var _ = Describe("Upgrade", Label("upgrade"), func() {
 
 		Expect(bosh.RunErrand(deploymentName, "smoke-tests", "mysql/first")).To(Succeed())
 
-		By("asserting cluster-health-logger is still able to connect to its local instance")
-		output, err := bosh.Logs(deploymentName, "mysql/0", "cluster-health-logger/cluster-health-logger.stderr.log")
-		Expect(err).NotTo(HaveOccurred())
-		Expect(output.String()).NotTo(ContainSubstring(`Access denied for user 'cluster-health-logger'`))
+		By("asserting pxc-5.7 actually went through crash recovery", func() {
+			output, err := bosh.Logs(deploymentName, "mysql/0", "pxc-mysql/pxc-57-recovery.log")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output.String()).To(ContainSubstring(`InnoDB: Starting crash recovery.`))
+		})
+
+		By("asserting cluster-health-logger is still able to connect to its local instance", func() {
+			output, err := bosh.Logs(deploymentName, "mysql/0", "cluster-health-logger/cluster-health-logger.stderr.log")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output.String()).NotTo(ContainSubstring(`Access denied for user 'cluster-health-logger'`))
+		})
 	})
 })
