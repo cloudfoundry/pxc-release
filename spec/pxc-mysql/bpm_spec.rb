f72887d1e24cc1f28c68f71385c65baee89d2472
Andrew Garner <garnera@vmware.com>
2022-12-05 19:40:04 -0600

    pxc-mysql: conditionally add percona-xtrabackup-2.4 to PATH for pxc-5.7
    When mysql_version is set to "5.7", ensure percona-xtrabackup-2.4 is on
    the PATH so Galera SST via the wsrep_xtrabackup-v2 script will continue
    to work.
    
    Backfilled some tests around this and cleaned up the bpm.yml templating.
    
    [#183907688](https://www.pivotaltracker.com/story/show/183907688)
    
    Authored-by: Andrew Garner <garnera@vmware.com>

--- spec/pxc-mysql/bpm_spec.rb
+++ spec/pxc-mysql/bpm_spec.rb
@@ -8,17 +8,24 @@ describe 'galera-agent-config template' do
   let(:job) { release.job('pxc-mysql') }
   let(:template) { job.template('config/bpm.yml') }
   let(:spec) { {} }
+  let(:rendered_template) { template.render(spec) }
+  let(:parsed_bpm_yaml) { YAML.load(rendered_template) }
 
-  it 'set default PATH for MySQL 8.0' do
-    tpl_output = template.render(spec)
-    hash_from_yaml = YAML.load(tpl_output)
-    expect(hash_from_yaml["processes"][0]["env"]["PATH"]).to eq('/usr/bin:/bin:/var/vcap/packages/percona-xtradb-cluster-8.0/bin/')
+  it 'set default PATH for Percona XtraDB Cluster 8.0' do
+    expect(parsed_bpm_yaml["processes"][0]["env"]["PATH"]).to eq('/usr/bin:/bin:/var/vcap/packages/percona-xtradb-cluster-8.0/bin')
   end
 
-  it 'set PATH for MySQL 5.7' do
-    spec["mysql_version"] = "5.7"
-    tpl_output = template.render(spec)
-    hash_from_yaml = YAML.load(tpl_output)
-    expect(hash_from_yaml["processes"][0]["env"]["PATH"]).to eq('/usr/bin:/bin:/var/vcap/packages/percona-xtradb-cluster-5.7/bin/')
+  context 'when mysql_version is explicitly set to 8.0' do
+    before { spec["mysql_version"] = "8.0" }
+    it 'set PATH for Percona XtraDB Cluster 5.7' do
+      expect(parsed_bpm_yaml["processes"][0]["env"]["PATH"]).to eq('/usr/bin:/bin:/var/vcap/packages/percona-xtradb-cluster-8.0/bin')
+    end
+  end
+
+  context 'when mysql_version is set to 5.7' do
+    before { spec["mysql_version"] = "5.7" }
+    it 'set PATH for Percona XtraDB Cluster 5.7' do
+      expect(parsed_bpm_yaml["processes"][0]["env"]["PATH"]).to eq('/usr/bin:/bin:/var/vcap/packages/percona-xtradb-cluster-5.7/bin:/var/vcap/packages/percona-xtrabackup-2.4/bin')
+    end
   end
 end
