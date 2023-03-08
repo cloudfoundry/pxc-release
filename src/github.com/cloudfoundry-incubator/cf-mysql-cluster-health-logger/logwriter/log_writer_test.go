472fdad835744589d3ee9bec6d9d5f39a4f16269
Andrew Garner <garnera@vmware.com>
2023-03-08 09:17:32 -0600

    cf-mysql-cluster-health-logger: bump to ginkgo/v2

--- src/github.com/cloudfoundry-incubator/cf-mysql-cluster-health-logger/logwriter/log_writer_test.go
+++ src/github.com/cloudfoundry-incubator/cf-mysql-cluster-health-logger/logwriter/log_writer_test.go
@@ -10,7 +10,7 @@ import (
 	"os"
 
 	"github.com/cloudfoundry-incubator/cf-mysql-cluster-health-logger/logwriter"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
