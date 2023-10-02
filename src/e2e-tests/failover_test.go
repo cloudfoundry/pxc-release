e69293036efbeedf7e33ca73355882c83b700430
Kyle Ong <kyleo@vmware.com>
2023-10-02 15:27:20 -0700

    failover_test: Only turn off resurrection for deploymentName
    [#185752726](https://www.pivotaltracker.com/story/show/185752726)
    
    Co-authored-by: Kyle Ong <kyleo@vmware.com>
    Co-authored-by: Ryan Wittrup <rwittrup@vmware.com>

--- src/e2e-tests/failover_test.go
+++ src/e2e-tests/failover_test.go
@@ -21,7 +21,7 @@ var _ = Describe("Failover", Ordered, Label("failover"), func() {
 		deploymentName = "pxc-failover-" + uuid.New().String()
 
 		Expect(cmd.Run(
-			"bosh", "update-resurrection", "off",
+			"bosh", "update-resurrection", "-d", deploymentName, "off",
 		)).To(Succeed())
 
 		Expect(bosh.DeployPXC(deploymentName,
