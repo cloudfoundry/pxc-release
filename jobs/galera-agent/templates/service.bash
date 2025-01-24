ba02c08c56049670dcb978e7c3b4ff15b49719b5
Andrew Garner <andrew.garner@broadcom.com>
2025-01-24 12:12:36 -0600

    Allow galera-agent access to monit under ubuntu noble
    This may be short lived, but fixes intended use of the bootstrap errand
    which coordinates with this agent to start MySQL instances in the
    correct order after a cluster has gone offline.

---
+++ jobs/galera-agent/templates/service.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o nounset
+
+if type -f -p nft >/dev/null && nft list ruleset | grep -q monit_output; then
+  rule_handle=$(nft -a list ruleset | awk '/galera-agent/ { print $NF }')
+  if [[ -n $rule_handle ]]; then
+    nft delete rule inet filter monit_output handle "${rule_handle}"
+  fi
+  nft insert rule inet filter monit_output index 2 \
+    socket cgroupv2 level 2 "system.slice/runc-bpm-galera-agent.scope" \
+    ip daddr 127.0.0.1 tcp dport 2822 \
+    log prefix '"Matched cgroup galera-agent monit access rule: "' \
+    accept
+elif [[ -f /var/vcap/bosh/etc/monit-access-helper.sh ]]; then
+  source /var/vcap/bosh/etc/monit-access-helper.sh
+  permit_monit_access
+fi
+
+# unmount fails under newer Ubuntu kernels without using the "--make-rslave" option
+# This affects the ubuntu-jammy stemcell 1.351 .. 1.390
+mount --make-rslave /sys/fs/cgroup
+umount --recursive /sys/fs/cgroup
+umount /var/vcap/bosh/etc
+exec chpst -u vcap -- /var/vcap/packages/galera-agent/bin/galera-agent \
+  --configPath=/var/vcap/jobs/galera-agent/config/galera-agent-config.yml \
+  --timeFormat="<%= p('logging.format.timestamp') %>"
