93fe5f25a6bc44fc865ccb381509ba7266e85cc4
Andrew Garner <andrew.garner@broadcom.com>
2025-12-03 09:38:53 -0600

    Move "pingdb" into switchboard package
    - Reorganize existing proxy main into conventional ./cmd/proxy/
      structure
    - Add ./cmd/pingdb/
    - cleanup packaging and testing scripts
    
    New dependencies:
    - github.com/go-sql-driver/mysql (used by pingdb to connect to MySQL)
    - github.com/google/uuid (used by pingdb test scripts to generate unique
      identifiers)
     - github.com/caarlos0/env/v11 (used by pingdb to parse configuration)

---
+++ src/github.com/cloudfoundry-incubator/switchboard/cmd/pingdb/main_suite_test.go
@@ -0,0 +1,29 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+
+	"testing"
+)
+
+var (
+	pingdbPath string
+)
+
+func TestPingDB(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "PingDB Suite")
+}
+
+var _ = BeforeSuite(func() {
+	var err error
+	pingdbPath, err = gexec.Build("github.com/cloudfoundry-incubator/switchboard/cmd/pingdb")
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	gexec.Kill()
+	gexec.CleanupBuildArtifacts()
+})
