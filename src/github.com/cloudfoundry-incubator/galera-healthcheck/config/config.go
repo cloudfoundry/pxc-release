218e140b4ea4cf8fc4f056e8a232efcb9ca32fc2
Kim Bassett <kim.bassett@broadcom.com>
2025-12-11 13:59:00 -0800

    galera-healthcheck: remove service-config package
    Parse config using standard go packages
    
    [TNZ-64710](https://vmw-jira.broadcom.net/browse/TNZ-64710)
    
    Authored-by: Kim Bassett <kim.bassett@broadcom.com>

--- src/github.com/cloudfoundry-incubator/galera-healthcheck/config/config.go
+++ src/github.com/cloudfoundry-incubator/galera-healthcheck/config/config.go
@@ -5,13 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"os"
 
 	"code.cloudfoundry.org/lager/v3"
 	"code.cloudfoundry.org/lager/v3/lagerflags"
 	"code.cloudfoundry.org/tlsconfig"
-	"github.com/pivotal-cf-experimental/service-config"
 	"github.com/pkg/errors"
 	"gopkg.in/validator.v2"
+	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
@@ -55,8 +56,8 @@ type EndpointTLS struct {
 	PrivateKey  string `yaml:"PrivateKey"`
 }
 
-func defaultConfig() *Config {
-	return &Config{
+func defaultConfig() Config {
+	return Config{
 		Host: "0.0.0.0",
 		Port: 8080,
 		DB: DBConfig{
@@ -74,19 +75,26 @@ func NewConfig(osArgs []string) (*Config, error) {
 
 	binaryName := osArgs[0]
 	configurationOptions := osArgs[1:]
-	serviceConfig := service_config.New()
+
+	// Define command line flags
 	flags := flag.NewFlagSet(binaryName, flag.ExitOnError)
+	var configData = flags.String("config", "", "json encoded configuration string")
+	var configPath = flags.String("configPath", "", "path to configuration file with json encoded content")
 
 	lagerflags.AddFlags(flags)
-
-	serviceConfig.AddFlags(flags)
-	serviceConfig.AddDefaults(defaultConfig())
 	flags.Parse(configurationOptions)
 
-	rootConfig.Logger, _ = lagerflags.NewFromConfig(binaryName, lagerflags.ConfigFromFlags())
+	// Start with defaults
+	rootConfig = defaultConfig()
 
-	err := serviceConfig.Read(&rootConfig)
-	return &rootConfig, err
+	// Load configuration from command line, file, or environment
+	err := loadConfig(&rootConfig, *configData, *configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	rootConfig.Logger, _ = lagerflags.NewFromConfig(binaryName, lagerflags.ConfigFromFlags())
+	return &rootConfig, nil
 }
 
 func (c Config) Validate() error {
@@ -132,4 +140,37 @@ func (c *Config) NetworkListener() (net.Listener, error) {
 	}
 
 	return tls.Listen("tcp", address, tlsConfig)
+}
+
+// Load configuration from sources in order of precedence:
+// command line data "-config" or file "-configPath", environment variables
+// for data CONFIG or file CONFIG_PATH.
+func loadConfig(config *Config, configData, configPath string) error {
+	var yamlData []byte
+	var err error
+
+	if configData != "" {
+		yamlData = []byte(configData)
+	} else if configPath != "" {
+		yamlData, err = os.ReadFile(configPath)
+		if err != nil {
+			return fmt.Errorf("failed to read config file %s: %w", configPath, err)
+		}
+	} else if envConfig := os.Getenv("CONFIG"); envConfig != "" {
+		yamlData = []byte(envConfig)
+	} else if envConfigPath := os.Getenv("CONFIG_PATH"); envConfigPath != "" {
+		yamlData, err = os.ReadFile(envConfigPath)
+		if err != nil {
+			return fmt.Errorf("failed to read config file from CONFIG_PATH %s: %w", envConfigPath, err)
+		}
+	} else {
+		return fmt.Errorf("no configuration provided: use -config, -configPath, CONFIG, or CONFIG_PATH")
+	}
+
+	// Parse YAML
+	if err := yaml.Unmarshal(yamlData, config); err != nil {
+		return fmt.Errorf("parsing YAML config: %w", err)
+	}
+
+	return nil
 }
