78462dcaf7a887020ebaaefc4896f871e03675d6
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2026-06-22 15:40:42 +0200

    chore(test): add option to start instances with tls
    we also want to test the tls encryption enabled scenario

--- src/pxc-replicator/testhelper/helper.go
+++ src/pxc-replicator/testhelper/helper.go
@@ -5,10 +5,16 @@ package testhelper
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"database/sql"
+	"encoding/pem"
 	"fmt"
 	"log"
-	"math/rand"
+	"math/big"
+	mathRand "math/rand"
 	"os"
 	"time"
 
@@ -89,9 +95,113 @@ type Log struct {
 	Content []byte
 }
 
-func StartContainerInstance(name, password, version string, netAliases []string, net *testcontainers.DockerNetwork) (fromContainer config.Target, fromHost config.Target) {
-	ctx := context.Background()
-	serverID := rand.Intn(999) + 1
+func writeKeyFile(path, name string) (key *rsa.PrivateKey, bytes []byte) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	Expect(err).ToNot(HaveOccurred())
+
+	file, err := os.Create(fmt.Sprintf("%s/%s", path, name))
+	defer file.Close()
+	Expect(err).ToNot(HaveOccurred())
+
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	}
+	log.Default().Printf("generated %s at %s", name, file.Name())
+
+	keyBytes := pem.EncodeToMemory(block)
+	_, err = file.Write(keyBytes)
+	Expect(err).ToNot(HaveOccurred())
+	return key, keyBytes
+}
+
+func writeCaFile(path, name string, caPrivateKey *rsa.PrivateKey) (*x509.Certificate, []byte) {
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(2026),
+		Subject: pkix.Name{
+			CommonName: name,
+		},
+		NotBefore:             time.Now().AddDate(0, 0, -1),
+		NotAfter:              time.Now().AddDate(0, 0, 1), // 10 years
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	certDERBytes, err := x509.CreateCertificate(rand.Reader, cert, cert, &caPrivateKey.PublicKey, caPrivateKey)
+	Expect(err).ToNot(HaveOccurred())
+
+	block := pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certDERBytes,
+	}
+	pemBytes := pem.EncodeToMemory(&block)
+	CAFile, err := os.Create(fmt.Sprintf("%s/%s", path, "server-ca.pem"))
+	Expect(err).ToNot(HaveOccurred())
+	_, err = CAFile.Write(pemBytes)
+
+	Expect(err).ToNot(HaveOccurred())
+
+	return cert, pemBytes
+}
+
+func writeCertFile(filename, path string, names []string, serverKeyPublic *rsa.PublicKey, caCert *x509.Certificate, caKey *rsa.PrivateKey) (*x509.Certificate, []byte) {
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			CommonName: names[0],
+		},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().AddDate(10, 0, 0),
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		DNSNames:    names,
+	}
+	certDERBytes, err := x509.CreateCertificate(rand.Reader, cert, caCert, serverKeyPublic, caKey)
+	Expect(err).ToNot(HaveOccurred())
+
+	block := pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certDERBytes,
+	}
+	pemBytes := pem.EncodeToMemory(&block)
+	serverCertFile, err := os.Create(fmt.Sprintf("%s/%s-%s", path, filename, "cert.pem"))
+	log.Default().Printf("wrote: %s", serverCertFile.Name())
+	Expect(err).ToNot(HaveOccurred())
+
+	_, err = serverCertFile.Write(pemBytes)
+	Expect(err).ToNot(HaveOccurred())
+
+	return cert, pemBytes
+}
+
+func InitCerts(name, path string, aliases []string) (serverCerts, clientCerts config.Certs) {
+	caKey, _ := writeKeyFile(path, "server-ca-key.pem")
+	serverCa, serverCABytes := writeCaFile(path, "localhost", caKey)
+
+	serverKey, serverKeyBytes := writeKeyFile(path, "server-key.pem")
+	_, serverCertBytes := writeCertFile("server", path, append([]string{"localhost", name}, aliases...), &serverKey.PublicKey, serverCa, caKey)
+
+	clientKey, clientKeyBytes := writeKeyFile(path, "client-key.pem")
+	_, clientCertBytes := writeCertFile("client", path, []string{"localhost", fmt.Sprintf("%s-client", name)}, &clientKey.PublicKey, serverCa, caKey)
+
+	clientCerts = config.Certs{
+		CA:          string(serverCABytes),
+		PrivateKey:  string(clientKeyBytes),
+		Certificate: string(clientCertBytes),
+	}
+	serverCerts = config.Certs{
+		CA:          string(serverCABytes),
+		PrivateKey:  string(serverKeyBytes),
+		Certificate: string(serverCertBytes),
+	}
+
+	return serverCerts, clientCerts
+}
+
+func StartContainerInstance(name, password, version string, tls bool, netAliases []string, net *testcontainers.DockerNetwork) (fromContainer config.Target, fromHost config.Target) {
+	serverID := mathRand.Intn(999) + 1
 	opts := []testcontainers.ContainerCustomizer{
 		network.WithNetwork(netAliases, net),
 		testcontainers.WithExposedPorts("3306"),
@@ -108,6 +218,34 @@ func StartContainerInstance(name, password, version string, netAliases []string,
 			wait.ForExposedPort().WithStartupTimeout(120*time.Second),
 		),
 	}
+	var clientCerts config.Certs
+	if tls {
+		certsDir, err := os.MkdirTemp("", name)
+		Expect(err).ToNot(HaveOccurred())
+		_, clientCerts = InitCerts(name, certsDir, netAliases)
+		opts = append(opts,
+			testcontainers.WithFiles(
+				testcontainers.ContainerFile{
+					HostFilePath:      fmt.Sprintf("%s/server-ca.pem", certsDir),
+					ContainerFilePath: "/certs/server-ca.pem",
+					FileMode:          0o777,
+				},
+				testcontainers.ContainerFile{
+					HostFilePath:      fmt.Sprintf("%s/server-cert.pem", certsDir),
+					ContainerFilePath: "/certs/server-cert.pem",
+					FileMode:          0o777,
+				},
+				testcontainers.ContainerFile{
+					HostFilePath:      fmt.Sprintf("%s/server-key.pem", certsDir),
+					ContainerFilePath: "/certs/server-key.pem",
+					FileMode:          0o644,
+				},
+			),
+			testcontainers.WithCmdArgs("--require-secure-transport=ON", "--ssl-cert=/certs/server-cert.pem", "--ssl-key=/certs/server-key.pem", "--ssl-ca=/certs/server-ca.pem"),
+		)
+	}
+
+	ctx := context.Background()
 	if os.Getenv("TEST_DEBUG") == "true" {
 		opts = append(opts, testcontainers.WithLogConsumerConfig(&testcontainers.LogConsumerConfig{
 			Opts:      []testcontainers.LogProductionOption{testcontainers.WithLogProductionTimeout(10 * time.Second)},
@@ -118,6 +256,7 @@ func StartContainerInstance(name, password, version string, netAliases []string,
 	pxc, err := testcontainers.Run(ctx, fmt.Sprintf("%s:%s", Image, version), opts...)
 
 	Expect(err).ToNot(HaveOccurred())
+
 	testcontainers.CleanupContainer(ginkgo.GinkgoTB(), pxc, testcontainers.StopTimeout(120*time.Second))
 	ip, err := pxc.ContainerIP(context.Background())
 	Expect(err).ToNot(HaveOccurred())
@@ -126,7 +265,6 @@ func StartContainerInstance(name, password, version string, netAliases []string,
 
 	// the networking with testcontainers makes this a bit hard.. we need to configure the replica with the "inner view" using the ContainerIP and the default 3306 port
 	// but to run external checks we need the Host view which is a mapped port on localhost...
-
 	return config.Target{
 			Name: name,
 			Host: ip,
@@ -135,7 +273,7 @@ func StartContainerInstance(name, password, version string, netAliases []string,
 				Username: "root",
 				Password: password,
 			},
-			TLS: config.Certs{},
+			TLS: clientCerts,
 		}, config.Target{
 			Name: name,
 			Host: "localhost",
@@ -144,5 +282,6 @@ func StartContainerInstance(name, password, version string, netAliases []string,
 				Username: "root",
 				Password: password,
 			},
+			TLS: clientCerts,
 		}
 }
