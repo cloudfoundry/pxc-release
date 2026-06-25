567148f1b87ba9d0974f818875b6ce9e72994c66
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2026-06-25 15:10:43 +0200

    chore(cleanup): small  cleanup
    - return early in stringer for state
      if we still see the default value of Enabled, then we can skip the rest..
    
    - the dump related log moved to the dumper
    
    - remove some newlines from the sql statements..
    
    - don't use pointer receivers. no need and at least this way we know there are no
      sideeffects by accident.

--- src/pxc-replicator/dumper/dumper_suite_test.go
+++ src/pxc-replicator/dumper/dumper_suite_test.go
@@ -1,16 +1,10 @@
 package dumper_test
 
 import (
-	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-)
-
-var (
-	mysqlBinDir = os.Getenv("MYSQL_BIN_DIR")
-	dataDir     = os.Getenv("DATA_DIR")
 )
 
 func TestDumper(t *testing.T) {
