1fdba475d046990b9b2baea8adddae00519206d8
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2026-07-06 18:08:15 +0200

    bug: apparently gtid can have multiple ranges

--- src/pxc-replicator/utils/utils_test.go
+++ src/pxc-replicator/utils/utils_test.go
@@ -7,21 +7,23 @@ import (
 )
 
 var _ = Describe("Utils", func() {
-	expectedVal := "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432"
 	DescribeTable("parsing gtids",
-		func(line string, shouldMatch bool) {
+		func(line string, shouldMatch bool, expected string) {
 			val, found := utils.ParseGTIDFromLine(line)
 			Expect(found).To(Equal(shouldMatch))
 			if shouldMatch {
-				Expect(val).To(Equal(expectedVal))
+				Expect(val).To(Equal(expected))
 			}
 		},
-		Entry("with comment", "SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '6ab3825e-e60b-11ed-a160-0242ac120002:1-15432';", true),
-		Entry("without comment", "SET @@GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002:1-15432';", true),
-		Entry("without the SET", "GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002:1-15432';", false),
-		Entry("missing 2nd @", "SET @GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002:1-15432';", false),
-		Entry("missing 2nd gtid element", "@@GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002';", false),
-		Entry("not a valid uuid", "SET @GLOBAL.GTID_PURGED='6ab3825e-11ed-a160-0242ac120002:1-15432';", false),
-		Entry("double range 2nd element", "SET @GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002:1-15432:1-22';", false),
+		Entry("with multiple ranges single uuid", "SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '6ab3825e-e60b-11ed-a160-0242ac120002:1-15432:2-85852';", true, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432:2-85852"),
+		Entry("with multiple ranges separate uuid", "SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '6ab3825e-e60b-11ed-a160-0242ac120002:1-15432,6ab3825e-e60b-11ed-a160-0242ac120003:2-85852';", true, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432,6ab3825e-e60b-11ed-a160-0242ac120003:2-85852"),
+		Entry("with multiple ranges separate uuid and space", "SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '6ab3825e-e60b-11ed-a160-0242ac120002:1-15432, 6ab3825e-e60b-11ed-a160-0242ac120003:2-85852';", true, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432,6ab3825e-e60b-11ed-a160-0242ac120003:2-85852"),
+		Entry("with comment", "SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '6ab3825e-e60b-11ed-a160-0242ac120002:1-15432';", true, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432"),
+		Entry("without comment", "SET @@GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002:1-15432';", true, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432"),
+		Entry("without the SET", "GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002:1-15432';", false, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432"),
+		Entry("missing 2nd @", "SET @GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002:1-15432';", false, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432"),
+		Entry("missing 2nd gtid element", "@@GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002';", false, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432"),
+		Entry("not a valid uuid", "SET @GLOBAL.GTID_PURGED='6ab3825e-11ed-a160-0242ac120002:1-15432';", false, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432"),
+		Entry("double range 2nd element", "SET @GLOBAL.GTID_PURGED='6ab3825e-e60b-11ed-a160-0242ac120002:1-15432:1-22';", false, "6ab3825e-e60b-11ed-a160-0242ac120002:1-15432"),
 	)
 })
